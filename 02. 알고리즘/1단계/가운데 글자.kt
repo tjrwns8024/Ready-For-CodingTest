2b483d38f979800eff46affff71eb629b0ea12e9
윤석준 <tjrwns8024@naver.com>
2021-01-31 16:30:20 +0900

    [CREATE] <1/ 가운데 글자.kt>

---
+++ 02. 알고리즘/1단계/가운데 글자.kt
@@ -0,0 +1,15 @@
+package programmers.one
+
+class Solution3 {
+    fun solution(s: String): String {
+        var answer = ""
+        if(s.length % 2 != 0){
+            
+        }
+        return answer
+    }
+}
+
+fun main() {
+
+}
